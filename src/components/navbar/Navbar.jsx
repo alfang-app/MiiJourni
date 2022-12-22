28385492db26a7f186217bcd47397076a39eaa80
alvin480 <kpfang@outlook.com>
2022-12-22 13:22:53 -0800

    Save all initial changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,10 +3,12 @@ import React, {useState} from 'react'
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import './navbar.css'
 import logo from '../../assets/logo.svg';
+
+import logo2 from '../../assets/images/logo/journi-logo.svg'
 const Menu = () => (
           <>
             <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#wgpt3">What is miJourni?</a></p>
             <p><a href="#possibility">Open AI</a></p>
             <p><a href="#features">Case Studies</a></p>
             <p><a href="#blog">Library</a></p>
@@ -20,7 +22,8 @@ const Navbar = () => {
     <div className ="gpt3__navbar">
       <div className="gpt3__navbar-links">
           <div className="gpt3__navbar-links_logo">
-              <img src={logo} alt="logo"/>
+        
+              <img src={logo2} alt="logo"/>
           </div>
 
 
